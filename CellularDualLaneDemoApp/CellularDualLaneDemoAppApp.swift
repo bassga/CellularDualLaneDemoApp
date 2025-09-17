a36f95694af137efacc4948e2d52fdd54a763821
basega <bassmky19940208@gmail.com>
2025-09-17 19:30:33 +0900

    Initial Commit

---
+++ CellularDualLaneDemoApp/CellularDualLaneDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularDualLaneDemoAppApp.swift
+//  CellularDualLaneDemoApp
+//
+//  Created by 奥川幹也 on 2025/09/17.
+//
+
+import SwiftUI
+
+@main
+struct CellularDualLaneDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CellularDualLaneDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CellularDualLaneDemoApp
+//
+//  Created by 奥川幹也 on 2025/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
